cbf7d8ac454ee1cef5fa8db35ddaa3fead4c76aa
Nontachai Yoothai <nontachai.y@gmail.com>
2018-10-16 22:34:02 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request, jsonify
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    jwt_refresh_token_required, create_refresh_token,
+    get_jwt_identity, set_access_cookies,
+    set_refresh_cookies, unset_jwt_cookies
+)
